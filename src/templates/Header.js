4516dba21bba054691c8ac7794ddcba5b2b18691
Juan Cruz Bidegain <juancruzbidegain@gmail.com>
2021-01-16 00:28:39 -0300

    first commit

---
+++ src/templates/Header.js
@@ -0,0 +1,23 @@
+const header = () => {
+    const view = `
+        <div class="Header-main">
+            <div class="Header-logo">
+                <h1>
+                    <a href="/">
+                        100tifi.co
+                    </a>
+                </h1>
+            </div>
+             <div class="Header-nav">
+                <h1>
+                    <a href="#/about">
+                        About
+                    </a>
+                </h1>
+            </div>
+        </div>
+    `;
+    return view;
+};
+
+export default header;
